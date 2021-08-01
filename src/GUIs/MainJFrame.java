b2146c7590f59eb449272cf9e6a4cc4ef8bd1b0b
James <Jtholloway3@gmail.com>
2021-08-01 10:54:58 +0100

    GUI Improvements

--- src/GUIs/MainJFrame.java
+++ src/GUIs/MainJFrame.java
@@ -13,14 +13,16 @@ public class MainJFrame extends javax.swing.JFrame {
 
     private static User CurrentUser;
 
-    private static int ScreenWidth;
-    private static int ScreenHeight;
-
+    private JFrame MainWindow;
     private static JPanel gamePanel;
-    private JPanel SideBar;
+
     private JLabel CloseWindowButton;
     private JLabel MinimiseWindowButton;
     private JLabel OpenPlayPanel;
+    private JLabel OpenLearnPanel;
+    private JLabel OpenAnalysisPanel;
+    private JLabel MoreButton;
+    private JLabel LogOutButton;
 
 
     public MainJFrame(GameType GameType, Colour PlayerColour, User user) {
@@ -28,96 +30,269 @@ public class MainJFrame extends javax.swing.JFrame {
         new GamePanel(GameType, PlayerColour);
 
         Initialise();
-        GamePressed();
-    }
-
-    //TODO test, add action lister
-    private static void GamePressed() {
-        gamePanel.setVisible(true);
-    }
-
-    public static User getCurrentUser() {
-        return CurrentUser;
     }
 
     private void Initialise() {
-        //Init Components
+        MainWindow = this;
+
+        //region Init Components
+        JPanel sideBar = new JPanel();
         gamePanel = GamePanel.getGameTab();
-        SideBar = new JPanel();
         CloseWindowButton = new JLabel();
         MinimiseWindowButton = new JLabel();
         OpenPlayPanel = new JLabel("Play  ", SwingConstants.RIGHT);
+        OpenLearnPanel = new JLabel("Learn  ", SwingConstants.RIGHT);
+        OpenAnalysisPanel = new JLabel("Analysis  ", SwingConstants.RIGHT);
+        MoreButton = new JLabel("More...  ", SwingConstants.RIGHT);
+        LogOutButton = new JLabel("Log Out", SwingConstants.CENTER);
+        //endregion
 
-        //Screen Dimensions
-        ScreenHeight = (Toolkit.getDefaultToolkit().getScreenSize().height);
-        ScreenWidth = (Toolkit.getDefaultToolkit().getScreenSize().width);
+        //region Screen Dimensions
+        int screenHeight = (Toolkit.getDefaultToolkit().getScreenSize().height);
+        int screenWidth = (Toolkit.getDefaultToolkit().getScreenSize().width);
+        //endregion
 
-        //Setup the Main Window
-        this.setSize(ScreenWidth, ScreenHeight);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null);
-        this.setLayout(null);
-        this.setUndecorated(true);
+        //region Setup the Main Window
+        MainWindow.setSize(screenWidth, screenHeight);
+        MainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        MainWindow.setLocationRelativeTo(null);
+        MainWindow.setLayout(null);
+        MainWindow.setUndecorated(true);
+        //endregion
 
-        //Setup the Menu Bar Panel (SideBar)
-        SideBar.setBounds(0, 0, (int) (ScreenWidth * 0.2d), ScreenHeight);
-        SideBar.setLayout(null);
-        SideBar.setBackground(new Color(64, 64, 64));
+        //region Setup the Menu Bar Panel (SideBar)
+        sideBar.setBounds(0, 0, (int) (screenWidth * 0.2d), screenHeight);
+        sideBar.setLayout(null);
+        sideBar.setBackground(Color.DARK_GRAY);
+        //endregion
 
-        //Setup Close Button
+        //region Setup Close Button
         CloseWindowButton.setText("X");
-        CloseWindowButton.setSize(ScreenHeight / 30, ScreenHeight / 26);
-        CloseWindowButton.setFont(new Font("CloseButton", Font.PLAIN, ScreenHeight / 26));
+        CloseWindowButton.setSize(screenHeight / 30, screenHeight / 26);
+        CloseWindowButton.setFont(new Font("CloseButton", Font.PLAIN, screenHeight / 26));
         CloseWindowButton.setForeground(Color.WHITE);
-        CloseWindowButton.setLocation((int) (SideBar.getWidth() * 0.05d), (int) (SideBar.getWidth() * 0.05d));
+        CloseWindowButton.setLocation((int) (sideBar.getWidth() * 0.1d), (int) (sideBar.getWidth() * 0.05d));
         CloseWindowButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
         CloseWindowButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
+                System.exit(0);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                if (CloseWindowButton.getForeground() == Color.WHITE) {
+                    CloseWindowButton.setForeground(Color.RED);
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                if (CloseWindowButton.getForeground() == Color.RED) {
+                    CloseWindowButton.setForeground(Color.WHITE);
+                }
             }
         });
+        //endregion
 
-        //Setup Minimise Button
+        //region Setup Minimise Button
         MinimiseWindowButton.setText("-");
-        MinimiseWindowButton.setSize(ScreenHeight / 30, ScreenHeight / 15);
-        MinimiseWindowButton.setFont(new Font("MinimiseButton", Font.BOLD, ScreenHeight / 15));
+        MinimiseWindowButton.setSize(screenHeight / 30, screenHeight / 15);
+        MinimiseWindowButton.setFont(new Font("MinimiseButton", Font.BOLD, screenHeight / 15));
         MinimiseWindowButton.setForeground(Color.WHITE);
-        MinimiseWindowButton.setLocation((int) (SideBar.getWidth() * 0.2d), (int) (SideBar.getWidth() * 0.008d));
+        MinimiseWindowButton.setLocation((int) (sideBar.getWidth() * 0.25d), (int) (sideBar.getWidth() * 0.008d));
         MinimiseWindowButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
         MinimiseWindowButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
+                MainWindow.setState(JFrame.ICONIFIED);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                if (MinimiseWindowButton.getForeground() == Color.WHITE) {
+                    MinimiseWindowButton.setForeground(Color.gray);
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                if (MinimiseWindowButton.getForeground() == Color.gray) {
+                    MinimiseWindowButton.setForeground(Color.WHITE);
+                }
             }
         });
+        //endregion
 
-        //Setup Play Button
-        OpenPlayPanel.setSize((int) (ScreenWidth * 0.2d), ScreenHeight / 26);
-        OpenPlayPanel.setFont(new Font("CloseButton", Font.PLAIN, ScreenHeight / 26));
+        //region Setup Play Button
+        OpenPlayPanel.setSize((int) (screenWidth * 0.2d), screenHeight / 26);
+        OpenPlayPanel.setFont(new Font("PlayButton", Font.PLAIN, screenHeight / 26));
         OpenPlayPanel.setForeground(Color.WHITE);
-        OpenPlayPanel.setLocation(0, (int) (SideBar.getHeight() * 0.15d));
+        OpenPlayPanel.setLocation(0, (int) (sideBar.getHeight() * 0.2d));
         OpenPlayPanel.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
         OpenPlayPanel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
+                if (!gamePanel.isVisible()) {
+                    gamePanel.setVisible(true);
+                    //TODO set other panels visibility to false
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                if (OpenPlayPanel.getForeground() == Color.WHITE) {
+                    OpenPlayPanel.setForeground(Color.gray);
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                if (OpenPlayPanel.getForeground() == Color.gray) {
+                    OpenPlayPanel.setForeground(Color.WHITE);
+                }
             }
         });
+        //endregion
 
-        //Add Components to Panels
-        SideBar.add(CloseWindowButton);
-        SideBar.add(MinimiseWindowButton);
-        SideBar.add(OpenPlayPanel);
+        //region Setup Learn Button
+        OpenLearnPanel.setSize((int) (screenWidth * 0.2d), screenHeight / 26);
+        OpenLearnPanel.setFont(new Font("LearnButton", Font.PLAIN, screenHeight / 26));
+        OpenLearnPanel.setForeground(Color.WHITE);
+        OpenLearnPanel.setLocation(0, (int) (sideBar.getHeight() * 0.28d));
+        OpenLearnPanel.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
-        //Add Panels to the JFrame
-        this.getContentPane().add(SideBar);
+        OpenLearnPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                if (OpenLearnPanel.getForeground() == Color.WHITE) {
+                    OpenLearnPanel.setForeground(Color.gray);
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                if (OpenLearnPanel.getForeground() == Color.gray) {
+                    OpenLearnPanel.setForeground(Color.WHITE);
+                }
+            }
+        });
+        //endregion
+
+        //region Setup Analysis Button
+        OpenAnalysisPanel.setSize((int) (screenWidth * 0.2d), screenHeight / 26);
+        OpenAnalysisPanel.setFont(new Font("AnalysisButton", Font.PLAIN, screenHeight / 26));
+        OpenAnalysisPanel.setForeground(Color.WHITE);
+        OpenAnalysisPanel.setLocation(0, (int) (sideBar.getHeight() * 0.36d));
+        OpenAnalysisPanel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        OpenAnalysisPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                if (OpenAnalysisPanel.getForeground() == Color.WHITE) {
+                    OpenAnalysisPanel.setForeground(Color.gray);
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                if (OpenAnalysisPanel.getForeground() == Color.gray) {
+                    OpenAnalysisPanel.setForeground(Color.WHITE);
+                }
+            }
+        });
+        //endregion
+
+        //region Setup More Button
+        MoreButton.setSize((int) (screenWidth * 0.2d), screenHeight / 26);
+        MoreButton.setFont(new Font("MoreButton", Font.PLAIN, screenHeight / 26));
+        MoreButton.setForeground(Color.WHITE);
+        MoreButton.setLocation(0, (int) (sideBar.getHeight() * 0.44d));
+        MoreButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        MoreButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                if (MoreButton.getForeground() == Color.WHITE) {
+                    MoreButton.setForeground(Color.gray);
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                if (MoreButton.getForeground() == Color.gray) {
+                    MoreButton.setForeground(Color.WHITE);
+                }
+            }
+        });
+        //endregion
+
+        //region Setup Logout Button
+        LogOutButton.setSize((int) (screenWidth * 0.2d), screenHeight / 18);
+        LogOutButton.setFont(new Font("LogoutButton", Font.PLAIN, screenHeight / 22));
+        LogOutButton.setForeground(Color.WHITE);
+        LogOutButton.setLocation(0, (int) (sideBar.getHeight() * 0.92d));
+        LogOutButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        LogOutButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                if (LogOutButton.getForeground() == Color.WHITE) {
+                    LogOutButton.setForeground(Color.gray);
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                if (LogOutButton.getForeground() == Color.gray) {
+                    LogOutButton.setForeground(Color.WHITE);
+                }
+            }
+        });
+        //endregion
+
+        //region Add Components to Panels
+        sideBar.add(CloseWindowButton);
+        sideBar.add(MinimiseWindowButton);
+        sideBar.add(OpenPlayPanel);
+        sideBar.add(OpenLearnPanel);
+        sideBar.add(OpenAnalysisPanel);
+        sideBar.add(MoreButton);
+        sideBar.add(LogOutButton);
+        //endregion
+
+        //region Add Panels to the JFrame
+        this.getContentPane().add(sideBar);
         this.getContentPane().add(gamePanel);
-
+        //endregion
 
         this.setVisible(true);
+    }
+
+    public static User getCurrentUser() {
+        return CurrentUser;
     }
 }
