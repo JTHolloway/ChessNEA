b97e1b07f60bfd0be25f95cd09e7e5385baaff5a
jh202075 <jtholloway3@gmail.com>
2021-10-21 17:21:35 +0100

    Quicksort

---
+++ src/LibaryFunctions/Quicksort.java
@@ -0,0 +1,15 @@
+package LibaryFunctions;
+import User.User;
+import java.util.List;
+
+public class Quicksort {
+
+    public static List<User> QuicksortByELO (List<User> UnsortedList){
+
+    }
+
+    public static void Quicksort(List<User> UnsortedList, ){
+
+    }
+
+}
