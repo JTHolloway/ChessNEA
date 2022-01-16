dafba6a4636d914067e266a742733d63cb869e9d
James <Jtholloway3@gmail.com>
2022-01-16 23:27:14 +0000

    Leaderboard Finished

--- src/GUIs/GUI_PodiumPanel.java
+++ src/GUIs/GUI_PodiumPanel.java
@@ -8,16 +8,16 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.List;
 
-public class GUI_PodiumPanel extends JPanel{
+public class GUI_PodiumPanel extends JPanel {
 
     private static List<User> Users;
 
     private static JTable ELOTable;
     private static JTable CountryTable;
+    private static JLabel WorldWide;
+    private static JLabel Country;
 
     /**
      * Constructor for the Podium JPanel. Calls the initComponents() method
@@ -28,35 +28,50 @@ public class GUI_PodiumPanel extends JPanel{
     }
 
     /**
-     * Updates the friends JTable to add their friends to the table
+     * Updates the ELO JTable to add the Users to the table
      */
     public static void UpdateELOTable() {
-        String[] ColumnNames = {"Username", "ELO"};
+        String[] ColumnNames = {"ELO", "Username", "Country"};
         DefaultTableModel model = new DefaultTableModel(ColumnNames, 0);
         ELOTable.setModel(model);
         model.addRow(ColumnNames);
 
+        Users = Repository.getUsers();
         Quicksort.quicksort(Users, true);
-        for (User user : Users){
-            String[] data = new String[2];
+        Users.removeIf(user -> user.getUserID().equals("Guest"));
+
+        List<User> topTenUsers;
+        if (Users.size() > 50) {
+            topTenUsers = Users.subList(0, 49);
+        } else topTenUsers = Users;
+        for (User user : topTenUsers) {
+            String[] data = new String[3];
             data[0] = String.valueOf(user.getStatistics().getELO());
             data[1] = user.getUserName();
+            data[2] = user.getCountry();
             model.addRow(data);
         }
     }
 
     /**
-     * Updates the friends JTable to add their friends to the table
+     * Updates the Country JTable to add the Users to the table
      */
     public static void UpdateCountryTable() {
-        String[] ColumnNames = {"Username", "ELO"};
+        String[] ColumnNames = {"ELO", "Username"};
         DefaultTableModel model = new DefaultTableModel(ColumnNames, 0);
         CountryTable.setModel(model);
         model.addRow(ColumnNames);
 
+        Users = Repository.getUsers();
         Quicksort.quicksort(Users, true);
-        Users.removeIf(user -> ! user.getCountry().equals(Repository.getCurrentUser().getCountry()));
-        for (User user : Users){
+        Users.removeIf(user -> !user.getCountry().equals(Repository.getCurrentUser().getCountry()));
+        Users.removeIf(user -> user.getUserID().equals("Guest"));
+
+        List<User> topTenUsers;
+        if (Users.size() > 50) {
+            topTenUsers = Users.subList(0, 49);
+        } else topTenUsers = Users;
+        for (User user : topTenUsers) {
             String[] data = new String[2];
             data[0] = String.valueOf(user.getStatistics().getELO());
             data[1] = user.getUserName();
@@ -75,31 +90,40 @@ public class GUI_PodiumPanel extends JPanel{
         this.setBackground(new Color(160, 160, 160));
         this.setLayout(null);
 
-        //init components
         ELOTable = new JTable();
         CountryTable = new JTable();
+        WorldWide = new JLabel("World Wide", SwingConstants.CENTER);
+        Country = new JLabel("Country", SwingConstants.CENTER);
 
-        //Search Results
+        WorldWide.setSize((int) (this.getWidth() * 0.45d), this.getHeight() / 26);
+        WorldWide.setFont(new Font("", Font.PLAIN, this.getHeight() / 26));
+        WorldWide.setForeground(Color.WHITE);
+        WorldWide.setLocation((int) (this.getWidth() * 0.1d), (int) (this.getHeight() * 0.05d));
+
+        Country.setSize((int) (this.getWidth() * 0.3d), this.getHeight() / 26);
+        Country.setFont(new Font("", Font.PLAIN, this.getHeight() / 26));
+        Country.setForeground(Color.WHITE);
+        Country.setLocation((int) (this.getWidth() - (this.getWidth() * 0.3d) - this.getWidth() * 0.1), (int) (this.getHeight() * 0.05d));
+
         ELOTable.setBorder(new LineBorder(Color.BLACK, 2));
         ELOTable.setBackground(Color.gray);
-        ELOTable.setForeground(Color.BLACK);
-        ELOTable.setBounds((int) (this.getWidth() * 0.1d), (int) (this.getHeight() * 0.1d), (int) (this.getWidth() * 0.3d),
+        ELOTable.setForeground(Color.WHITE);
+        ELOTable.setBounds((int) (this.getWidth() * 0.1d), (int) (this.getHeight() * 0.1d), (int) (this.getWidth() * 0.45d),
                 (int) (this.getHeight() * 0.8d));
-        ELOTable.setFont(new Font("", Font.PLAIN, (int) (this.getHeight() * 0.03d)));
-        ELOTable.setRowHeight((int) (this.getHeight() * 0.03d));
-        //TODO actionListener
+        ELOTable.setFont(new Font("", Font.PLAIN, (int) (this.getHeight() * 0.02d)));
+        ELOTable.setRowHeight((int) (this.getHeight() * 0.02d));
 
-        //Friend List
         CountryTable.setBorder(new LineBorder(Color.BLACK, 2));
         CountryTable.setBackground(Color.gray);
-        CountryTable.setForeground(Color.BLACK);
+        CountryTable.setForeground(Color.WHITE);
         CountryTable.setBounds((int) (this.getWidth() - (this.getWidth() * 0.3d) - this.getWidth() * 0.1),
                 (int) (this.getHeight() * 0.1d), (int) (this.getWidth() * 0.3d), (int) (this.getHeight() * 0.8d));
-        CountryTable.setFont(new Font("", Font.PLAIN, (int) (this.getHeight() * 0.03d)));
-        CountryTable.setRowHeight((int) (this.getHeight() * 0.03d));
-        //TODO actionListener
+        CountryTable.setFont(new Font("", Font.PLAIN, (int) (this.getHeight() * 0.02d)));
+        CountryTable.setRowHeight((int) (this.getHeight() * 0.02d));
 
         this.add(ELOTable);
         this.add(CountryTable);
+        this.add(WorldWide);
+        this.add(Country);
     }
 }
