5b1f5317167dd2fedf3e628d3cf391e5bb1ddac8
James <Jtholloway3@gmail.com>
2022-01-20 00:43:14 +0000

    Interactive Board

--- src/GUIs/GUI_BoardPanel.java
+++ src/GUIs/GUI_BoardPanel.java
@@ -17,7 +17,8 @@ import java.util.List;
 public class GUI_BoardPanel extends JPanel {
 
     private List<Tile> Tiles;
-    private Square selectedSquare;
+    private Square selectedSquare = null;
+    private Square destinationSquare;
 
     /**
      * Constructor for the board JPanel
@@ -30,11 +31,15 @@ public class GUI_BoardPanel extends JPanel {
 
         InitialiseTiles();
         InitialisePieces();
-
+//
 //        Square[][] array = GUI_GamePanel.getGame().getBoard().getBoardArray();
 //
 //        GUI_GamePanel.getGame().getBoard().PrintBoard();
 //        Game.MakeMove(new Move.RegularMove(array[1][1], array[2][1]), GUI_GamePanel.getGame().getBoard());
+//        Game.MakeMove(new Move.RegularMove(array[0][2], array[2][0]), GUI_GamePanel.getGame().getBoard());
+//        Game.MakeMove(new Move.RegularMove(array[0][1], array[2][2]), GUI_GamePanel.getGame().getBoard());
+//        Game.MakeMove(new Move.RegularMove(array[1][4], array[3][4]), GUI_GamePanel.getGame().getBoard());
+//        Game.MakeMove(new Move.RegularMove(array[0][3], array[2][5]), GUI_GamePanel.getGame().getBoard());
 //        System.out.println();
 //        GUI_GamePanel.getGame().getBoard().PrintBoard();
 //
@@ -53,7 +58,7 @@ public class GUI_BoardPanel extends JPanel {
 
         for (int i = 1; i <= 8; i++) {
             for (int j = 1; j <= 8; j++) {
-                    tile = new Tile(game.getBoard().getBoardArray()[(7- (i - 1))][(7- (j - 1))]);
+                tile = new Tile(game.getBoard().getBoardArray()[(7- (i - 1))][(7- (j - 1))]);
 
                 if (i % 2 != j % 2) {
                     tile.setBackground(new Color(234, 182, 118));
@@ -73,25 +78,29 @@ public class GUI_BoardPanel extends JPanel {
                 tile.addMouseListener(new MouseAdapter() {
                     @Override
                     public void mouseClicked(MouseEvent e) {
-                        //todo if selected square is not already equal to null then count it as a destination, otherwise count as an origin
-                        //todo can only move a piece if selected square is occupied, of the colour of the player and it is the PLAYERS TURN
-                        selectedSquare = finalTile.getSquare();
+                        //todo can only move a piece if it is the PLAYERS TURN
 
-                        if (selectedSquare.SquareOccupied()) {
-                            if (selectedSquare.ReturnPiece().getColour() == game.getSelectedColour()) {
-                                List<Move> moves = selectedSquare.ReturnPiece().CalculateValidMoves(game.getBoard());
-                                for (Tile tile1 : Tiles) {
-                                    tile1.setBorder(null);
-                                }
+                        if (selectedSquare == null) { //Selecting a square
+                            SelectOriginSquare(finalTile, game);
 
+                        } else { //Selecting a destination
+                            List<Move> moves = selectedSquare.ReturnPiece().CalculateValidMoves(game.getBoard());
+                            boolean valid = SelectDestinationSquare(finalTile, game, moves);
+
+                            if (valid) {
+                                previousBorder[0] = null;
                                 for (Move move : moves) {
                                     for (Tile tile1 : Tiles) {
                                         if (tile1.getSquare().ReturnCoordinate().CompareCoordinates(move.getEndPosition())) {
-                                            tile1.setBorder(new LineBorder(Color.GREEN, 5));
+                                            tile1.setBorder(null);
                                         }
                                     }
                                 }
+                            } else {
+                                SelectOriginSquare(finalTile, game);
                             }
+                            destinationSquare = null;
+                            selectedSquare = null;
                         }
                     }
 
@@ -114,17 +123,55 @@ public class GUI_BoardPanel extends JPanel {
         }
     }
 
-   private void InitialisePieces() {
-       int TileSize = (this.getSize().height) / 8;
+    private void SelectOriginSquare(Tile finalTile, Game game) {
+        selectedSquare = finalTile.getSquare();
+        if (selectedSquare.SquareOccupied()) { //If the square is occupied
+            if (selectedSquare.ReturnPiece().getColour() == game.getSelectedColour()) { //then if the piece is the players piece DO..
+                List<Move> moves = selectedSquare.ReturnPiece().CalculateValidMoves(game.getBoard());
+                for (Tile tile1 : Tiles) {
+                    tile1.setBorder(null);
+                }
+                for (Move move : moves) {
+                    for (Tile tile1 : Tiles) {
+                        if (tile1.getSquare().ReturnCoordinate().CompareCoordinates(move.getEndPosition())) {
+                            tile1.setBorder(new LineBorder(Color.GREEN, 5));
+                        }
+                    }
+                }
+            } else selectedSquare = null;
+        } else selectedSquare = null;
+    }
 
-       for (Tile tile : Tiles) {
-           JLabel PieceIcon = new JLabel();
-           PieceIcon.setSize(TileSize, TileSize);
-           if (tile.getSquare().SquareOccupied()){
-               if (tile.getSquare().ReturnPiece().getColour() == Colour.WHITE){
-                   PieceIcon.setForeground(new Color(247, 229, 195));
-               } else PieceIcon.setForeground(new Color(59, 40, 4));
-               String Icon = tile.getSquare().ReturnPiece().ReturnPieceIcon();
+    private boolean SelectDestinationSquare(Tile finalTile, Game game, List<Move> moveList) {
+        destinationSquare = finalTile.getSquare();
+        boolean valid = false;
+        A:
+        for (Move move : moveList) {
+            for (Tile tile1 : Tiles) {
+                if (move.getEndPosition().ReturnCoordinate().CompareCoordinates(destinationSquare)) {
+                    valid = true;
+                    Game.MakeMove(move, game.getBoard());
+                    //todo update board
+                    System.out.println();
+                    game.getBoard().PrintBoard();
+                    break A;
+                }
+            }
+        }
+        return valid;
+    }
+
+    private void InitialisePieces() {
+        int TileSize = (this.getSize().height) / 8;
+
+        for (Tile tile : Tiles) {
+            JLabel PieceIcon = new JLabel();
+            PieceIcon.setSize(TileSize, TileSize);
+            if (tile.getSquare().SquareOccupied()) {
+                if (tile.getSquare().ReturnPiece().getColour() == Colour.WHITE) {
+                    PieceIcon.setForeground(new Color(247, 229, 195));
+                } else PieceIcon.setForeground(new Color(59, 40, 4));
+                String Icon = tile.getSquare().ReturnPiece().ReturnPieceIcon();
                PieceIcon.setText(Icon);
            } else {
                String Icon = "";
