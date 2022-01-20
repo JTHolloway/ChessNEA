e4d7f1d2643907981408b0b5668d9b9bee868f4e
James <Jtholloway3@gmail.com>
2022-01-20 16:17:41 +0000

    Tile class contains the tile icon

--- src/GUIs/Tile.java
+++ src/GUIs/Tile.java
@@ -9,17 +9,29 @@ import javax.swing.*;
  */
 public final class Tile extends JPanel {
     private final Square square;
+    private final JLabel icon;
 
     /**
      * Constructor for a displayable board tile.
      *
      * @param square The square which the tile refers to
      */
-    public Tile(final Square square) {
+    public Tile(final Square square, int tileSize) {
         this.square = square;
+        this.icon = new JLabel("");
+        this.icon.setSize(tileSize, tileSize);
+        this.add(icon);
     }
 
     public Square getSquare() {
         return square;
+    }
+
+    public JLabel getIcon() {
+        return icon;
+    }
+
+    public void setIcon(JLabel icon) {
+        this.add(icon);
     }
 }
