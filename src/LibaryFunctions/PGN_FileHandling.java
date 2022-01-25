e0a4bf353b47197393a241fa86d2a86d1bf99b03
jh202075 <jtholloway3@gmail.com>
2022-01-25 11:21:08 +0000

    New Class to handle PGN file

---
+++ src/LibaryFunctions/PGN_FileHandling.java
@@ -0,0 +1,25 @@
+package LibaryFunctions;
+
+import java.io.File;
+
+public class PGN_FileHandling {
+
+    private File PGN_File;
+
+    public static void createPGN(){
+
+    }
+
+    public static void writeToPGN(String StringToWrite){
+
+    }
+
+    public static String readFromPGN(){
+        return "";
+    }
+
+    public static void storePGN(){
+
+    }
+
+}
