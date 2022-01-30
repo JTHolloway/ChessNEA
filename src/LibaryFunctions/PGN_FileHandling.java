0d01e37b035e79be5ae931927ab16e917588f3e9
James <Jtholloway3@gmail.com>
2022-01-30 17:08:33 +0000

    Files can be created and written to

--- src/LibaryFunctions/PGN_FileHandling.java
+++ src/LibaryFunctions/PGN_FileHandling.java
@@ -1,25 +1,48 @@
 package LibaryFunctions;
 
+import GUIs.GUI_GamePanel;
+import Game.GameType;
+import Game.Colour;
+
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Calendar;
 
 public class PGN_FileHandling {
 
-    private File PGN_File;
+    private static File PGN_File;
 
     public static void createPGN(){
+        String FileName = Repository.getCurrentUser().getUserID() + "_" + Repository.getCurrentUser().getStatistics().getGames() + ".txt";
+        PGN_File = new File(FileName);
 
+        try {
+            PGN_File.createNewFile();
+
+        } catch (IOException exception) {
+            System.out.println("Error Creating PGN file");
+            exception.printStackTrace();
+        }
+
+        writeToPGN("[Event \"" + GUI_GamePanel.getGame().getGameType() + "\"]\n");
+        writeToPGN("[Date \"" + new java.sql.Date (Calendar.getInstance().getTime().getTime()) + "\"]\n");
+        writeToPGN("[User Elo \"" + Repository.getCurrentUser().getStatistics().getELO() + "\"]\n");
+        writeToPGN("[User \"" + Repository.getCurrentUser().getUserID() + "\"]\n\n\n");
     }
 
     public static void writeToPGN(String StringToWrite){
+        try {
+            FileWriter fileWriter = new FileWriter(PGN_File.getName(), true);
+            fileWriter.write(StringToWrite);
+            fileWriter.close();
 
-    }
-
-    public static String readFromPGN(){
-        return "";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void storePGN(){
 
     }
-
 }
