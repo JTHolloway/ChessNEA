10cac024b81f4424eb6c114dc9b1b750e4e9c111
jh202075 <jtholloway3@gmail.com>
2022-02-10 17:09:01 +0000

    Comments

--- src/LibaryFunctions/MultiThread.java
+++ src/LibaryFunctions/MultiThread.java
@@ -21,14 +21,23 @@ public class MultiThread implements Runnable {
      */
     @Override
     public void run() {
+        //Calculate minimax move
         Move computerMove = minimax.getComputerMove(Colour.GetOtherColour(GUI_GamePanel.getGame().getSelectedColour()));
+
+        //Update GUI to display move notation
         GUI_GamePanel.updateMoveBox(GUI_GamePanel.getGame().MoveToNotation(computerMove));
+
+        //Update the number of total moves made
         GUI_GamePanel.getGame().setMovesMade(GUI_GamePanel.getGame().getMovesMade() + 1);
 
+        //Make the move and update the board GUI
         Game.MakeMove(computerMove, GUI_GamePanel.getGame().getBoard());
         GUI_BoardPanel.UpdateBoard();
 
+        //Set the player as the player to move
         GUI_GamePanel.getGame().UpdatePlayerToMove();
+
+        //Update the text which tells the user whose turn it is on the game GUI
         GUI_GamePanel.updateTurn();
     }
 }
